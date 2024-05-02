41585ce555963155e6af5b89a5f7a97ab616d2de
shrey2109 <spparikh2109@gmail.com>
2024-05-02 12:32:07 +0530

    fix: added insights

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import './index.css';
 
@@ -12,6 +13,7 @@ root.render(
   <>
     <Provider store={store}>
       <App />
+      <SpeedInsights />
     </Provider>
   </>
 );
